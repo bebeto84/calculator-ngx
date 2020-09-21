66617f231ba11e2a624cc767f0abe1f8c4e60fda
bebeto84 <msolervlc@gmail.com>
2020-09-21 12:37:40 +0200

    feat(form): initialize Form with calculator input and use custom validation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,3 +1,5 @@
+import { evaluate } from 'mathjs';
+import { CustomValidators } from './../utils/validators';
 import {
   Component,
   OnInit,
@@ -5,7 +7,30 @@ import {
   ChangeDetectionStrategy,
   HostBinding,
 } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
 @Component({
   selector: 'calculator-form',
   templateUrl: './form.component.html',
@@ -16,7 +41,33 @@ import {
 export class FormComponent implements OnInit {
   @HostBinding('class') hostClass = 'calculator-form';
 
-  constructor() {}
+  form = this.initForm();
+
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  onEnter(): void {
+    this.calculatorControl.setValue(evaluate(this.calculatorControl.value));
+  }
+
+  get calculatorControl(): AbstractControl | undefined {
+    return this.form.controls['calculator'];
+  }
+
+  private initForm(): FormGroup {
+    return this.fb.group({
+      calculator: [
+        '',
+        [
+          CustomValidators.allowedCharacters,
+          CustomValidators.balancedParentheses,
+          CustomValidators.expressionWithParentheses,
+          CustomValidators.evaluateCalculation,
+        ],
+      ],
+    });
+  }
 }
--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { FormComponent } from './form.component';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FormComponent],
-  imports: [CommonModule, MatInputModule],
+  imports: [CommonModule, MatInputModule, ReactiveFormsModule],
   providers: [],
 })
 export class FormModule {}
